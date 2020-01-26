fe7f402ba7e70bff521b527613c90d1f903c715f
elysonsousa <elyson.sousa@outlook.com>
2020-01-26 15:09:36 -0300

    Enviando para o repositório e testando o firebase

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+
+
+  Firestore.instance.collection("mensagens").document().setData({
+    "texto": "td bem",
+    "from": "Elyson",
+    "read": "false"
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
